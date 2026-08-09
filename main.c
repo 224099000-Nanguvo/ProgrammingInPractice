f8844dd3749d891d4fe9d7f782c458bef51b7dbd
224099000-Nanguvo <224099000@nust.na>
2026-08-09 21:30:04 +0200

    Week 1 - Municipal Financial Management System

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
 int main(){
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    printf("Municipal Financial Management System\n Welcome to Windhoek Municipality\n\n");
+
+    printf("Enter Municipaly Name: ");
+    scanf(" %49[^\n]", municipality);
+
+    printf("Enter Mayor: ");
+    scanf(" %49[^\n]", mayor);
+
+    printf("Enter Population: ");
+    scanf("%d", &population);
+
+    printf("\n...........................\n");
+    printf("Municipality: %s\n", municipality);
+    printf("Mayor: %s\n", mayor);
+    printf("Population: %d\n", population);
+
     return 0;
 }
